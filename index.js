010bf0a08d2efb701fe9395ada23aebf2e76a739
Mkrah <amirou.kaba26@compscihigh.org>
2025-07-15 11:04:22 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const app = express()
+
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path)
+  next()
+})
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
+
+app.listen(3000, ()=>{
+  console.log("Server started...")
+})
